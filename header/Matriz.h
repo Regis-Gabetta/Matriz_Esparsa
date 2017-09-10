6e71704b1e77a37080819390673bb8155cd7edc2
Raigous <regis_gabetta@hotmail.com>
2017-09-10 20:48:09 -0300

    operators funcionando belamente

--- header/Matriz.h
+++ header/Matriz.h
@@ -9,49 +9,56 @@
 using namespace std;
 
 template<typename T>
-class Matriz{
+class Matriz {
 
- public:
+	friend class Vetor<T>;
+public:
 	Matriz(T t0);
 	~Matriz();
 	Vetor<T>* get(unsigned int i);
-	
 	Vetor<T> & operator [](unsigned int i);
 
- private:
+private:
 	T default0;
 	Vetor< Vetor<T>* > vetor;
 	Vetor<T>* temp;
+	int linAnt = -1;
 };
 
 
 template<typename T>
-Matriz<T>::Matriz(T d0){
+Matriz<T>::Matriz(T d0) {
 	default0 = d0;
 	Vetor<T>* dp0 = NULL;
 	vetor = Vetor< Vetor<T>* >(dp0);
+	temp = (Vetor<T>*)malloc(sizeof(Vetor<T>));
 }
 
 template<typename T>
-Matriz<T>::~Matriz(){
+Matriz<T>::~Matriz() {
 }
 
 template<typename T>
 Vetor<T>* Matriz<T>::get(unsigned int i) {
 	Vetor<T>* vp = vetor.get(i);
-	
-	if (vp == NULL){
+
+	if (vp == NULL) {
 		Vetor<T>* v = new Vetor<T>(default0);
 		vetor.set(i, v);
 		return v;
 	}
-	
+
 	return vp;
 }
 
+
 template<typename T>
-Vetor<T> & Matriz<T>::operator [](unsigned int i){
+Vetor<T> & Matriz<T>::operator [](unsigned int i) {
+	if (temp->getCol() != -1 && linAnt != -1)
+		this->get(linAnt)->set(temp->getCol(),temp->getTemp());
 	temp = get(i);
+	buf->setLin(i);
+	linAnt = i;
 	return *temp;
 }
 
--- header/Vetor.h
+++ header/Vetor.h
@@ -10,48 +10,60 @@
 using namespace std;
 
 template<typename T>
-class Vetor{
+class Vetor {
 
- public:
+public:
 	Vetor(T t0);
+//	Vetor(T t0, Matriz<T> mat);
 	Vetor();
 	~Vetor();
 	void set(unsigned int i, T o);
+	int getCol() const;
 	T get(unsigned int i) const;
-
+	T getTemp() const;
 	T operator [](unsigned int  i) const;
-  T & operator [](unsigned int  i);
-	
- private:
+	T & operator [](unsigned int  i);
+
+private:
 	T default0;
 	Arvore<No <T> > arv;
 	T temp;
+	int col;
+//	Matriz<T> m;
 };
 
+//template<typename T>
+//Vetor<T>::Vetor(T t0, Matriz<T> mat) {
+//	default0 = t0;
+//	m = mat;
+//	arv = Arvore<No <T> >();
+//}
 
 template<typename T>
-Vetor<T>::Vetor(T t0){
+Vetor<T>::Vetor(T t0) {
 	default0 = t0;
 	arv = Arvore<No <T> >();
+	col = -1;
 }
 
 template<typename T>
-Vetor<T>::Vetor(){
+Vetor<T>::Vetor() {
+	col = -1;
 }
 
 template<typename T>
-Vetor<T>::~Vetor(){
+Vetor<T>::~Vetor() {
 }
 
 template<typename T>
-void Vetor<T>::set(unsigned int i, T o){
-	
-	if (o != default0){
+void Vetor<T>::set(unsigned int i, T o) {
+
+	if (o != default0) {
 		No<T> n = No<T>(o, i);
 		arv.insert(n);
 	}
-	
-	else{
+
+	else {
 		No<T> n = No<T>(i);
 		arv.remove(n);
 	}
@@ -61,23 +73,36 @@ template<typename T>
 T Vetor<T>::get(unsigned int i) const {
 	No<T> n = No<T>(i);
 	No<T> ng = arv.get(n);
-	
+
 	No<T> n0 = No<T>(-1);
-	
+
 	if (ng != n0)
 		return ng.info;
-	
+
 	return default0;
 }
 
+
 template<typename T>
-T Vetor<T>::operator [](unsigned int i) const{
+T Vetor<T>::getTemp() const {
+	return temp;
+}
+
+template<typename T>
+int Vetor<T>::getCol() const {
+	return col;
+}
+
+template<typename T>
+T Vetor<T>::operator [](unsigned int i) const {
+
 	return this->get(i);
 }
 
 template<typename T>
-T & Vetor<T>::operator [](unsigned int i){
+T & Vetor<T>::operator [](unsigned int i) {
 	temp = this->get(i);
+	col = i;
 	return temp;
 }
 
